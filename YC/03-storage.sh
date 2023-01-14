19978250cd14e62dd271c2e0698875c0e7f60118
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-01-14 21:16:13 +0400

    day-04

--- YC/03-storage.sh
+++ YC/03-storage.sh
@@ -6,6 +6,13 @@ yc storage bucket create \
   --name my-first-bucket-by-cli \
   --max-size 2 
 
+# get list of buckets
+yc storage bucket list
 
+# get list of buckets
+yc storage bucket list
 
+# get info about bucket
+yc storage bucket get my-first-bucket-by-cli
 
+# upload file using AWS cli
