e547db3a525230a03fb4da590a5bbde68c549e93
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-01-16 19:13:19 +0400

    day-06

--- YC/03-storage.sh
+++ YC/03-storage.sh
@@ -20,21 +20,45 @@ yc storage bucket list
 # get info about bucket
 yc storage bucket get my-first-bucket-by-cli
 
+####
 # upload file using AWS cli
 # but before create service account, assign role and create static access key
+####
 # create a service account
 yc iam service-account create --name my-service-account \
   --description "this is my service account"
 # get list of roles
 yc iam role list
+# get list of folders
+yc resource-manager folder list
 # assigning roles to service account
-yc iam service-account set-access-bindings my-service-account \
-  --access-binding role=storage.uploader,subject=serviceAccount:ajedlav2drium5si0p21 \
-  --access-binding role=storage.viewer,subject=serviceAccount:ajedlav2drium5si0p21
-# show roles of service account
-yc iam service-account list-access-bindings my-service-account
+# we will assign role to whole folder (bacause we can't assign role to bucket via cli)
+yc resource-manager folder add-access-binding default \
+  --role storage.uploader \
+  --subject serviceAccount:ajedlav2drium5si0p21
+# show assigned roles 
+yc resource-manager folder list-access-binding default \
 # create static key
 yc iam access-key create --service-account-name my-service-account \
   --description "this key is for my bucket"
-# infroramtion from previous command needed for configure AWS CLI
-# 
+
+# infroramtion from previous command needed to configure AWS CLI
+# aws cli configuration https://cloud.yandex.com/en-ru/docs/storage/tools/aws-cli
+# upload all objects from a local directory
+aws --endpoint-url=https://storage.yandexcloud.net \
+  s3 cp --recursive ~/to_s3/app s3://my-first-bucket-by-cli/app/
+
+# getting list of object 
+aws --endpoint-url=https://storage.yandexcloud.net \
+  s3 ls s3://my-first-bucket-by-cli/app/
+
+# delete object (role "storage.uploader" is not enough for delete)
+aws --endpoint-url=https://storage.yandexcloud.net \
+  s3 rm s3://my-first-bucket-by-cli/app/loki.yml
+
+# retrieving an object
+aws --endpoint-url=https://storage.yandexcloud.net \
+  s3 cp s3://my-first-bucket-by-cli/app/mongo.yml ~/to_s3/mongo.yml
+
+# show bucket stats
+yc storage bucket stats my-first-bucket-by-cli
