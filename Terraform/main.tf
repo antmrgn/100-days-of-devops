452d16517934402b7613240f2b4b38a3fc2b4c88
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-01-20 19:07:57 +0400

    day-10

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -12,8 +12,8 @@ provider "yandex" {
 }
 
 
-resource "yandex_compute_instance" "vm-by-terraform-1" {
-  name = "terraform1"
+resource "yandex_compute_instance" "vm-by-terraform-1-centos" {
+  name = "vm-by-terraform-1-centos"
 
   resources {
     cores  = 2
@@ -22,68 +22,76 @@ resource "yandex_compute_instance" "vm-by-terraform-1" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd87va5cc00gaq2f5qfb"
+      image_id = "fd8151sv1q69mchl804a"
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = yandex_vpc_subnet.subnet-by-terraform.id
     nat       = true
   }
 
   metadata = {
     user-data = "${file("./meta.txt")}"
   }
+
+  scheduling_policy {
+    preemptible = true
+  }
 }
 
-resource "yandex_compute_instance" "vm-2" {
-  name = "terraform2"
+resource "yandex_compute_instance" "vm-by-terraform-2-ubuntu" {
+  name = "vm-by-terraform-2-ubuntu"
 
   resources {
-    cores  = 4
+    cores  = 2
     memory = 4
   }
 
   boot_disk {
     initialize_params {
-      image_id = "fd87va5cc00gaq2f5qfb"
+      image_id = "fd8b24tqvq7t2f8a1o1s"
     }
   }
 
   network_interface {
-    subnet_id = yandex_vpc_subnet.subnet-1.id
+    subnet_id = yandex_vpc_subnet.subnet-by-terraform.id
     nat       = true
   }
 
   metadata = {
     user-data = "${file("./meta.txt")}"
   }
+  
+  scheduling_policy {
+    preemptible = true
+  }  
 }
 
-resource "yandex_vpc_network" "network-1" {
-  name = "network1"
+resource "yandex_vpc_network" "network-by-terraform" {
+  name = "network-by-terraform"
 }
 
-resource "yandex_vpc_subnet" "subnet-1" {
-  name           = "subnet1"
+resource "yandex_vpc_subnet" "subnet-by-terraform" {
+  name           = "subnet-by-terraform"
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network-1.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
+  network_id     = yandex_vpc_network.network-by-terraform.id
+  v4_cidr_blocks = ["192.168.11.0/24"]
 }
 
 output "internal_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+  value = yandex_compute_instance.vm-by-terraform-1-centos.network_interface.0.ip_address
 }
 
 output "internal_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+  value = yandex_compute_instance.vm-by-terraform-2-ubuntu.network_interface.0.ip_address
 }
 
 
 output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.vm-by-terraform-1-centos.network_interface.0.nat_ip_address
 }
 
 output "external_ip_address_vm_2" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+  value = yandex_compute_instance.vm-by-terraform-2-ubuntu.network_interface.0.nat_ip_address
 }
