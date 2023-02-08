2522b128b67de6116bba6ece154c0840e874bf39
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-02-08 19:03:40 +0400

    day-27

---
+++ Python/exercism/14-locomotive_engineer.py
@@ -0,0 +1,68 @@
+"""Functions which helps the locomotive engineer to keep track of the train."""
+
+
+# TODO: define the 'get_list_of_wagons' function
+def get_list_of_wagons(*args):
+    """Return a list of wagons.
+
+    :param: arbitrary number of wagons.
+    :return: list - list of wagons.
+    """
+    return list(args)
+
+
+# TODO: define the 'fixListOfWagons()' function
+def fix_list_of_wagons(each_wagons_id, missing_wagons):
+    """Fix the list of wagons.
+
+    :parm each_wagons_id: list - the list of wagons.
+    :parm missing_wagons: list - the list of missing wagons.
+    :return: list - list of wagons.
+    """
+    
+    [a, b, c, *one] = each_wagons_id
+    [*two] = missing_wagons
+    listOfWagons = [c]
+    listOfWagons.extend(two)
+    listOfWagons.extend(one)
+    return listOfWagons + [a, b]
+
+
+# TODO: define the 'add_missing_stops()' function
+def add_missing_stops(*args, **kwargs):
+    """Add missing stops to route dict.
+
+    :param route: dict - the dict of routing information.
+    :param: arbitrary number of stops.
+    :return: dict - updated route dictionary.
+    """
+    stop_list=[]
+    for key, value in kwargs.items():
+        stop_list.extend({value})
+    
+    list_missing_stops = args
+    
+    return {stops: stop_list}
+
+
+
+# TODO: define the 'extend_route_information()' function
+def extend_route_information(route, more_route_information):
+    """Extend route information with more_route_information.
+
+    :param route: dict - the route information.
+    :param more_route_information: dict -  extra route information.
+    :return: dict - extended route information.
+    """
+    return {**route, **more_route_information}
+
+
+# TODO: define the 'fix_wagon_depot()' function
+def fix_wagon_depot(wagons_rows):
+    """Fix the list of rows of wagons.
+
+    :param wagons_rows: list[list[tuple]] - the list of rows of wagons.
+    :return: list[list[tuple]] - list of rows of wagons.
+    """
+    list = zip(*wagons_rows)
+    return list
