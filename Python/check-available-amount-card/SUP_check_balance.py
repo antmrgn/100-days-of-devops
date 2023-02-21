91d2d00ca080bdd7ace2a5a5d9a85d95b869e78c
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-02-21 20:06:38 +0400

    day-39

---
+++ Python/check-available-amount-card/SUP_check_balance.py
@@ -0,0 +1,14 @@
+# взято из https://xakep.ru/2021/11/28/python-telegram-bots/
+import telebot
+# Создаем экземпляр бота
+bot = telebot.TeleBot('bot_key')
+# Функция, обрабатывающая команду /start
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
