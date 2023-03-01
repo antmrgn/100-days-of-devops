10bf943878262309b17ed35cb1ceb7e44525ccc1
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-03-01 19:56:20 +0400

    day-47

--- Python/exercism/23-isogram.py
+++ Python/exercism/23-isogram.py
@@ -1,4 +1,10 @@
 def is_isogram(string):
+    d = dict(string)
+    print(d)
+#    print(string[1])
     for item in string:
-        if string.count(item) == 1:
-            
+        if string.count(item) > 1:
+            return 0
+        return 1
+
+print(is_isogram('worddd'))
