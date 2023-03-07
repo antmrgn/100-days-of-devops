d1d502a9e435937f5dfa5e97a7d4ad81541f8b54
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-03-07 17:45:19 +0400

    day-52

--- Python/exercism/26-bob.py
+++ Python/exercism/26-bob.py
@@ -1,8 +1,12 @@
+'''Bob's responses'''
 def response(hey_bob):
-    if hey_bob[-1] == '?':
-        return "Sure."
-    elif hey_bob[-1] == '!':
-        return "Calm down, I know what I'm doing!"
-    elif hey_bob == "":
+    hey_bob = hey_bob.strip(" ")
+    if hey_bob == "" or hey_bob.isspace():
         return "Fine. Be that way!"
+    elif hey_bob[-1] == '?':
+        if hey_bob.isupper():
+            return "Calm down, I know what I'm doing!"
+        return "Sure."
+    elif hey_bob.isupper():
+        return "Whoa, chill out!"
     return "Whatever."
