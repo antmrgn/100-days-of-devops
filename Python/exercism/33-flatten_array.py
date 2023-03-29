5e49dfbd121ec4f43f0cc1faad4913d314a6ac74
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-03-29 19:20:48 +0400

    day-74

--- Python/exercism/33-flatten_array.py
+++ Python/exercism/33-flatten_array.py
@@ -8,10 +8,20 @@ def flatten(iterable):
                 print(i)
                 mylist.append(i)
             else:
-                flatten(i)
+                mylist.append(flatten2(i))
                 print("else")
                 print(i)
     return mylist
 
+def flatten2(iterable):
+    for i in iterable:
+#        if i != "null" or i != "nil" or i is not None:
+        if type(i) is not list:
+            print("if flatten2")
+            print(i)
+            return i
+        else:
+            print("else flatten2")
+            return flatten2(i)
 
 print(flatten([1, [2, [[3]], [4, [[5]]], 6, 7], 8]))
