d255b1c6ec7caf6d2b1e31144db303783411ce00
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-04-06 17:31:56 +0400

    day-81

--- Python/check-available-amount-card/test-aiogram.py
+++ Python/check-available-amount-card/test-aiogram.py
@@ -1,58 +1,24 @@
 import asyncio
 import logging
-
-from aiogram import Bot, Dispatcher, Router, types
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
 from config_reader import config
 
-# Bot token can be obtained via https://t.me/BotFahter
-TOKEN = "my-token"
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config.bot_token.get_secret_value())
+# Диспетчер
+dp = Dispatcher()
 
-# All handlers should be attached to the Router (or Dispatcher)
-router = Router()
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
 
-
-@router.message(Command(commands=["start"]))
-async def command_start_handler(message: Message) -> None:
-    """
-    This handler receive messages with `/start` command
-    """
-    # Most event objects have aliases for API methods that can be called in events' context
-    # For example if you want to answer to incoming message you can use `message.answer(...)` alias
-    # and the target chat will be passed to :ref:`aiogram.methods.send_message.SendMessage`
-    # method automatically or call API method directly via
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
-    await message.answer(f"Hello, <b>{message.from_user.full_name}!</b>")
-
-
-@router.message()
-async def echo_handler(message: types.Message) -> None:
-    """
-    Handler will forward received message back to the sender
-
-    By default, message handler will handle all message types (like text, photo, sticker and etc.)
-    """
-    try:
-        # Send copy of the received message
-        await message.send_copy(chat_id=message.chat.id)
-    except TypeError:
-        # But not all the types is supported to be copied so need to handle it
-        await message.answer("Nice try!")
-
-
-async def main() -> None:
-    # Dispatcher is a root router
-    dp = Dispatcher()
-    # ... and all other routers should be attached to Dispatcher
-    dp.include_router(router)
-
-    # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    bot = Bot(TOKEN, parse_mode="HTML")
-    # And the run events dispatching
+# Запуск процесса поллинга новых апдейтов
+async def main():
     await dp.start_polling(bot)
 
-
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
