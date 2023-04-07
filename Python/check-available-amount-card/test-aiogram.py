2c40157a076fb748de000adfc650b4cf84571f7c
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-04-07 19:46:48 +0400

    day-82

--- Python/check-available-amount-card/test-aiogram.py
+++ Python/check-available-amount-card/test-aiogram.py
@@ -3,6 +3,7 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 from config_reader import config
+import requests
 
 # Включаем логирование, чтобы не пропустить важные сообщения
 logging.basicConfig(level=logging.INFO)
@@ -14,7 +15,31 @@ dp = Dispatcher()
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer("Введи номер телефона в формате +7XXXXXXXXXX и через пробел четыре последние цифры карты")
+
+@dp.message(Command("help"))
+async def process_help_command(message: types.Message):
+    await message.answer("Бот поможет тебе узнать баланс карты SUP. Напиши /start чтобы начать")
+
+@dp.message()
+async def echo_message(message: types.Message):
+#    await message.answer("message")
+#    await message.answer(message.text)
+                         
+    phone, card_number = message.text.split()
+
+    url = f"https://meal.gift-cards.ru/api/1/virtual-cards/{phone}/{card_number}"
+
+    response = requests.get(url).json()
+ 
+    # Извлекаем данные баланса карты
+    balance = response['data']['balance']['availableAmount']
+
+    # Отправляем пользователю сообщение с балансом карты
+    await message.answer("Баланс карты:") 
+    await message.answer(balance)   
+
+
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
