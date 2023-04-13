23c56e6f1b9627757a7a8dc1f35778daff11ab1d
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-04-13 20:07:28 +0400

    day-88

--- Python/exercism/36-gigasecond.py
+++ Python/exercism/36-gigasecond.py
@@ -1,16 +1,10 @@
-from datetime import timedelta
+import time
+import datetime
+from datetime import date
 
+gigasecond = 1000000000
 def add(moment):
+    pass
 
->>> year = timedelta(days=365)
->>> ten_years = 10 * year
->>> ten_years
-datetime.timedelta(days=3650)
->>> ten_years.days // 365
-10
->>> nine_years = ten_years - year
->>> nine_years
-datetime.timedelta(days=3285)
->>> three_years = nine_years // 3
->>> three_years, three_years.days // 365
-(datetime.timedelta(days=1095), 3)
+today = date.today()
+print(today)
