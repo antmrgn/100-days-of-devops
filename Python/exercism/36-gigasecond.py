2e8524327b2931353f70c3349b08b101f1360707
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-04-14 19:40:01 +0400

    day-89

--- Python/exercism/36-gigasecond.py
+++ Python/exercism/36-gigasecond.py
@@ -1,10 +1,31 @@
 import time
-import datetime
-from datetime import date
+import datetime 
+
+#from datetime import date
+
+#from datetime import datetime
 
 gigasecond = 1000000000
-def add(moment):
-    pass
+#def add(moment):
+#    print(moment)
+#    return datetime.datetime(moment).timestamp()
 
-today = date.today()
-print(today)
+#moment = (2011, 4, 25, 0, 0)
+moment = datetime.datetime(2011, 4, 25, 0, 0)
+#moment = datetime.datetime(2011, 4, 25, 0, 0).timestamp()
+unix = time.mktime(moment.timetuple())
+print(unix)
+res = int(unix + gigasecond)
+print(res)
+
+#from datetime import datetime
+#nice = datetime.utcfromtimestamp(res)
+nice = datetime.datetime.fromtimestamp(res)
+#nice = time.mktime(res.timetuple())
+print(nice)
+
+
+#today = date.today()
+#print(today)
+
+#print(add((2011, 4, 25, 0, 0)))
