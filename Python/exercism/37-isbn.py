145a73bc6dae67d4855b6dcdf00a163455e118cb
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-04-19 20:29:48 +0400

    day-94

--- Python/exercism/37-isbn.py
+++ Python/exercism/37-isbn.py
@@ -1,10 +1,13 @@
 def is_valid(isbn):
     a = isbn.replace('-', '')
     print(list(a))
-    if len(a) == 10:
-        b = int(a[0]) * 10 + int(a[1]) * 9 + int(a[2]) * 8 + int(a[3]) * 7 + int(a[4]) * 6 + int(a[5]) * 5 + int(a[6]) * 4 + int(a[7]) * 3 + int(a[8]) * 2 + int(a[9]) * 1
-        if b % 11 == 0:
-            return True
+    if len(a) == 10 and (a.isdigit() or "X" in a):
+        if a.isdigit():
+            b = int(a[0]) * 10 + int(a[1]) * 9 + int(a[2]) * 8 + int(a[3]) * 7 + int(a[4]) * 6 + int(a[5]) * 5 + int(a[6]) * 4 + int(a[7]) * 3 + int(a[8]) * 2 + int(a[9]) * 1
+            if b % 11 == 0:
+                return True
+            elif "X" in a:
+                
     return False
 
 print(is_valid("3-598-21508-8"))
