c06c96edc442761f4ca6fc97af38752b55ca66ac
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-04-22 08:49:47 +0400

    day-97

--- Python/exercism/37-isbn.py
+++ Python/exercism/37-isbn.py
@@ -2,15 +2,18 @@ def is_valid(isbn):
     a = isbn.replace('-', '')
     print(list(a))
     sum = 0 
+    k = 10
     if len(a) == 10:
         if a.isdigit():
             print("a is digit")
             for i in list(a):
-                for n in range(10, 0, -1):
-                    sum = sum + int(i) * n
+#                for n in range(10, 0, -1):
+                print("for range. i = " + str(i) + " k = " + str(k))
+                sum = sum + int(i) * k
+                k = k - 1
             if sum % 11 == 0:
                 return True
-        elif "X" in a:
+        elif "X" in a[9]:
             print("elif X")
             a = list(a)
             for i, n in enumerate(a):
@@ -18,15 +21,21 @@ def is_valid(isbn):
                     a[i] = 10
             print(a)
             print(type(a[3]))
-            if all(isinstance(e, (int)) for e in a):
+#            if all(isinstance(e, (int)) for e in a):
+            b = ''.join(str(x) for x in a)
+            print(b)
+            if all(ele.isdigit() for ele in b):
                 print("check int")
                 print(a)
+
                 for i in a:
-                    for n in range(10, 0, -1):
-                        sum = sum + int(i) * n
+#                    for n in range(10, 0, -1):
+                    print("for range. i = " + str(i) + " k = " + str(k))
+                    sum = sum + int(i) * k
+                    k = k - 1
                 if sum % 11 == 0:
                     return True
                 
     return False
 
-print(is_valid("3-598-21507-X"))
+print(is_valid("3-598-21515-X"))
