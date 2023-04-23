7693ede0c4a09a776818e8c313cd51ac4f5e324e
Anton Morgenshtern <tonchyk2006@yandex.ru>
2023-04-23 08:46:03 +0400

    day-98

--- Python/exercism/37-isbn.py
+++ Python/exercism/37-isbn.py
@@ -1,41 +1,26 @@
+# verify ISBN-10 both with and without separating dashes
 def is_valid(isbn):
-    a = isbn.replace('-', '')
-    print(list(a))
     sum = 0 
     k = 10
-    if len(a) == 10:
-        if a.isdigit():
-            print("a is digit")
-            for i in list(a):
-#                for n in range(10, 0, -1):
-                print("for range. i = " + str(i) + " k = " + str(k))
+
+# убираем дефисы
+    isbnnew = isbn.replace('-', '')
+
+# проверяем длину строки
+    if len(isbnnew) == 10:
+        # смотрим есть ли "X". Если есть то заменяем на 10
+        if "X" in isbnnew[9]:
+            isbnnew = list(isbnnew)
+            isbnnew[9] = 10
+
+        # склеиваем список в строку. Нужно для проверки элементов
+        isbnstr = ''.join(str(x) for x in isbnnew)
+        # проверяем что все элементы в строке являются цифрами
+        if all(ele.isdigit() for ele in isbnstr):
+            # виладируем ISBN-10 по формуле
+            for i in isbnnew:
                 sum = sum + int(i) * k
                 k = k - 1
             if sum % 11 == 0:
                 return True
-        elif "X" in a[9]:
-            print("elif X")
-            a = list(a)
-            for i, n in enumerate(a):
-                if n == "X":
-                    a[i] = 10
-            print(a)
-            print(type(a[3]))
-#            if all(isinstance(e, (int)) for e in a):
-            b = ''.join(str(x) for x in a)
-            print(b)
-            if all(ele.isdigit() for ele in b):
-                print("check int")
-                print(a)
-
-                for i in a:
-#                    for n in range(10, 0, -1):
-                    print("for range. i = " + str(i) + " k = " + str(k))
-                    sum = sum + int(i) * k
-                    k = k - 1
-                if sum % 11 == 0:
-                    return True
-                
     return False
-
-print(is_valid("3-598-21515-X"))
